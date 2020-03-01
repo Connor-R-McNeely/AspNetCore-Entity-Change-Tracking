1f5d59a646b79c0285317ec55e6afc1407993143
Connor McNeely <connor.r.mcneely@ou.edu>
2020-02-29 22:28:13 -0600

    ITimestamp and ApplicationDbContext
    Added ITimestamp
    - This provides an interface for which CreatedDateTime, CreatedBy, EditedDateTime, and EditedBy can be tracked.
    
    Added overrides in ApplicationDbContext
    - HttpContextAccessor injected to allow for current username retrieval
    - Syncronous and Asyncronous methods provided
    - Handles the updating of all properties in ITimestamp in order to keep controllers free of this burden

--- EntityChangeTracking_AspNetCore-3.0/EntityChangeTracking_AspNetCore-3.0/Data/ApplicationDbContext.cs
+++ EntityChangeTracking_AspNetCore-3.0/EntityChangeTracking_AspNetCore-3.0/Data/ApplicationDbContext.cs
@@ -1,16 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using EntityChangeTracking_AspNetCore_3._0.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using EntityChangeTracking_AspNetCore_3._0.Models;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EntityChangeTracking_AspNetCore_3._0.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor httpContextAccessor)
             : base(options)
         {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        private IHttpContextAccessor _httpContextAccessor;
+
+        public override int SaveChanges()
+        {
+            UpdateTimestamps();
+            return base.SaveChanges();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            UpdateTimestamps();
+            return await base.SaveChangesAsync();
+        }
+
+        public void UpdateTimestamps()
+        {
+            var iTimestampEntities = ChangeTracker.Entries()
+                .Where(x => x.Entity is ITimestamp)
+                .Where(x => x.State == EntityState.Added || x.State == EntityState.Modified);
+
+            var userName = _httpContextAccessor.HttpContext.User?.Identity?.Name;
+
+            userName = userName ?? "System";
+
+            foreach (var e in iTimestampEntities.Where(x => x.State == EntityState.Added))
+            {
+                ((ITimestamp)e.Entity).CreatedDateTime = DateTime.Now;
+                ((ITimestamp)e.Entity).CreatedBy = userName;
+            }
+
+            foreach (var e in iTimestampEntities.Where(x => x.State == EntityState.Modified))
+            {
+                e.Property("CreatedDateTime").IsModified = false;
+                e.Property("CreatedBy").IsModified = false;
+                ((ITimestamp)e.Entity).EditedDateTime = DateTime.Now;
+                ((ITimestamp)e.Entity).EditedBy = userName;
+            }
         }
     }
 }
---
+++ EntityChangeTracking_AspNetCore-3.0/EntityChangeTracking_AspNetCore-3.0/Models/Interfaces/ITimestamp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityChangeTracking_AspNetCore_3._0.Models.Interfaces
+{
+    interface ITimestamp
+    {
+        public DateTime CreatedDateTime { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? EditedDateTime { get; set; }
+        public string EditedBy { get; set; }
+    }
+}
